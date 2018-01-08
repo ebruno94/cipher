05f530883641993632761ceb45414fafdc836909
Ernest Bruno and Miranda Keith <ernest@>
2018-01-08 14:20:43 -0800

    Added more functions and two images in webpage.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,3 @@
-/*$(document).ready(function() {
-  $("h2").click(function() {
-    // FIRST FUNCTION
-    function takeSentence() {
-      var sentence = prompt("Type a sentence: ")
-      alert("Your sentence is: " sentence);
-    };
-
-    function firstLast(sentence) {
-      var firstChar = sentence.toUpperCase().charAt(0);
-      var lastChar = sentence.toUpperCase().charAt(sentence.length - 1);
-      alert(firstChar + lastChar);
-      return firstChar + lastChar;
-    };
-
-    takeSentence();
-    firstLast(sentence);
-
-  });
-
-});
-*/
 function takeSentence(sentence){
   alert("Your sentence is: " + sentence);
 };
@@ -38,7 +16,56 @@ function reverse(chars){
   return lastChar + firstChar;
 }
 
-var sentence = prompt("Please type a sentence: ");
-takeSentence(sentence);
-var charFirstAndLast = firstLast(sentence);
-var charReverse = reverse(charFirstAndLast);
+function middleChar(sentence, newSentence){
+  var letterCount = sentence.length;
+  var middle = letterCount / 2;
+  middle = Math.floor(middle);
+  alert(middle);
+  newSentence = sentence[middle]+newSentence;
+  alert(newSentence);
+  return newSentence;
+}
+
+function reverseSentence(newSentence){
+  var text = "";
+  for(i = newSentence.length-1; i > -1; i--){
+    text += newSentence[i];
+  }
+  alert(text);
+}
+
+function start(){
+  var sentence = prompt("Please type a sentence: ");
+  takeSentence(sentence);
+  var charFirstAndLast = firstLast(sentence);
+  var charReverse = reverse(charFirstAndLast);
+  var newSentence = sentence + charFirstAndLast;
+  alert(newSentence);
+  var newNewSentence = middleChar(sentence, newSentence);
+  reverseSentence(newNewSentence);
+
+}
+
+
+$(document).ready(function() {
+  $("gif").click(function() {
+    // FIRST FUNCTION
+    function takeSentence() {
+      var sentence = prompt("Type a sentence: ")
+      alert("Your sentence is: " sentence);
+    };
+  });
+
+  $("pic").click(function() {
+    function firstLast(sentence) {
+      var firstChar = sentence.toUpperCase().charAt(0);
+      var lastChar = sentence.toUpperCase().charAt(sentence.length - 1);
+      alert(firstChar + lastChar);
+      return firstChar + lastChar;
+    };
+  });
+
+});
+
+
+start();
